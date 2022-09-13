7762e32d619213f2caf935f0e2907e0e839dcf34
sylvestereo <soputa42@gmail.com>
2022-09-13 07:54:40 -0700

    I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints alphabet to terminal
+ * print_alphabet - print alphabet lowercase
  *
- * Description: Prints the letters of the english alphabet
- * in lowercase.
- * Return: Void
+ * Return: Always 0.
  */
 
 void print_alphabet(void)
 {
-	char letter = 'a';
+char alphabet;
 
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		letter++;
-	}
-	_putchar('\n');
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+_putchar('\n');
+
 }
